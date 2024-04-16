0ff618a16f1bea6c119af0878d2bede269358948
ad-astra-per-ardua <sinnarver15@gmail.com>
2024-04-16 22:28:25 +0900

    Section 2

--- Section2/scripts/app.js
+++ Section2/scripts/app.js
@@ -1,3 +1,16 @@
-import {apikey} from './util.js'
+function debugging() {
+    console.log('Testing.') // Declaring function
+}
+debugging(); // Calling function.
 
-console.log(apikey)
+function printing(parameterA, parameterB = 'Default value.') {
+    console.log(parameterA)
+    console.log(parameterB)
+}
+printing('tempA'); // parameterB will replace by default value.
+printing('testA', 'testB'); // Can override default value.
+
+function returning(paramA, paramB) {
+    return "Rewinding JavaScript By " + paramA + ' On, ' + paramB + '.'
+}
+console.log(returning('rlatjdals', '4/16'))
